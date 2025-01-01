501001efc780bc0c47f3f576b37b471cda577005
Matthew Piskunov <matthewpiskunov@yahoo.com>
2025-01-01 17:44:45 -0500

    removed portfolio site

--- porfolio-site/src/app/directives/gridbreaks.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GridbreaksDirective } from './gridbreaks.directive';
-
-describe('GridbreaksDirective', () => {
-  it('should create an instance', () => {
-    // const directive = new GridbreaksDirective();
-    // expect(directive).toBeTruthy();
-  });
-});
