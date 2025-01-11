90772f49bbf613172a23631c019008826a9b0a60
Matthew Piskunov <matthewpiskunov@yahoo.com>
2025-01-10 19:21:35 -0500

    updated resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -9,7 +9,7 @@ const ResumePDFPage = () => {
       <HeaderSection isSticky={false} />
       <Box sx={{ width: "100%" }}>
         <Stack direction="row" justifyContent="center" alignItems="center" sx={{ height: "100vh", minHeight: "700px" }}>
-          <object data="/resume2024.pdf" type="application/pdf" width="100%" height="100%" />
+          <object data="/MatthewPiskunov_Resume.pdf" type="application/pdf" width="100%" height="100%" />
         </Stack>
       </Box>
     </>
